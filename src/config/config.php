b71e81448b8ac619aa01a8fdd188615269c46bb0
Ash <ashley@chichesterdesign.co.uk>
2014-06-10 11:56:51 +0100

    Committed required config file

---
+++ src/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+	'templates' => array(
+		
+		/*
+		|--------------------------------------------------------------------------
+		| Template Dir
+		|--------------------------------------------------------------------------
+		|
+		| The directory used to store template files.
+		| Relative to the path of the active theme.
+		|
+		*/
+
+		'dir' => 'templates'
+		
+	)
+
+);
